1f90c0fd45cf43f256eb6d1121dce9a02a095f6e
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2021-03-06 00:13:08 +0100

    #68

---
+++ pages/events/[id].js
@@ -0,0 +1,11 @@
+import { useRouter } from "next/router";
+
+export default function EventPage() {
+  const router = useRouter();
+
+  return (
+    <div>
+      <h1>Event Detail</h1>
+    </div>
+  );
+}
