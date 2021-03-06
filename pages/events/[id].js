1e516b283ff4603c3132f43dadbfae2f04eb9fbc
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2021-03-06 14:40:09 +0100

    #74

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,11 +1,31 @@
+import { Fragment } from "react";
 import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
 
 export default function EventPage() {
   const router = useRouter();
+  const eventId = router.query.id;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
 
   return (
-    <div>
-      <h1>Event Detail</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   );
 }
