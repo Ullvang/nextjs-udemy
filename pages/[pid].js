185b589e884c2070c80a5b7b4ba459cb8e1400d1
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2021-03-07 13:22:59 +0100

    #94

---
+++ pages/[pid].js
@@ -0,0 +1,26 @@
+import path from "path";
+import fs from "fs";
+
+export default function ProductDetailPage({ loadedProduct }) {
+  return (
+    <>
+      <h1>{loadedProduct.title}</h1>
+      <p>{loadedProduct.description}</p>
+    </>
+  );
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const productId = params.pid;
+
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFileSync(filePath);
+  const data = JSON.parse(jsonData);
+  const product = data.products.find((product) => product.id === productId);
+  return {
+    props: {
+      loadedProduct: product,
+    },
+  };
+}
