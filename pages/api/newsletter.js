a3871b19998c8939553dfa4666de28f0ca105314
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2021-03-13 12:14:25 +0100

    #157

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,23 @@
-import { MongoClient } from "mongodb";
+import { connect, MongoClient } from "mongodb";
+
+async function connectDatabase() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://nextjs-app:" +
+      process.env.DB_PASS +
+      "@nextjs-db.mnf8i.mongodb.net/events?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
+  return client;
+}
+
+async function insertDocument(client, document) {
+  const db = client.db();
+  await db.collection("newsletter").insertOne(document);
+  return db;
+}
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
@@ -9,21 +28,22 @@ export default async function handler(req, res) {
       return;
     }
 
-    const client = await MongoClient.connect(
-      "mongodb+srv://nextjs-app:" +
-        process.env.DB_PASS +
-        "@nextjs-db.mnf8i.mongodb.net/events?retryWrites=true&w=majority",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
-    const db = client.db();
-    await db.collection("newsletter").insertOne({ email: userEmail });
+    let client;
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({ message: "Connecting to the database failed!" });
+      return;
+    }
 
-    client.close();
+    try {
+      await insertDocument(client, { email: userEmail });
+      client.close();
+    } catch (error) {
+      res.status(500).json({ message: "Inserting data failed!" });
+      return;
+    }
+
     res.status(201).json({ message: "Signed up!" });
   }
 }
-
-//mongodb+srv://nextjs-app:nextjs-app123@nextjs-db.mnf8i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
