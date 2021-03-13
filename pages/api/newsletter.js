967c4da5a40266b259c96553bdd7bdbc896938ce
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2021-03-13 11:30:29 +0100

    #153

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,6 @@
-export default function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+export default async function handler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
 
@@ -7,7 +9,21 @@ export default function handler(req, res) {
       return;
     }
 
-    console.log(userEmail);
+    const client = await MongoClient.connect(
+      "mongodb+srv://nextjs-app:" +
+        process.env.DB_PASS +
+        "@nextjs-db.mnf8i.mongodb.net/newsletter?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    const db = client.db();
+    await db.collection("emails").insertOne({ email: userEmail });
+
+    client.close();
     res.status(201).json({ message: "Signed up!" });
   }
 }
+
+//mongodb+srv://nextjs-app:nextjs-app123@nextjs-db.mnf8i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
