4e552f012e227f6ec2aecb0ff136a51df15aa42e
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2021-03-13 13:59:08 +0100

    #167

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,6 +1,6 @@
 import { Fragment, useContext } from "react";
 import MainHeader from "./main-header";
-import Notification from "../components/ui/notification";
+import Notification from "../../components/ui/notification";
 import NotificationContext from "../../store/notification-context";
 
 function Layout(props) {
