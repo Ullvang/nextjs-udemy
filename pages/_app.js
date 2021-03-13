99ac92bef45cf7479fe968510b6cebc8b40b1078
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2021-03-13 13:36:02 +0100

    #116

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import "../styles/globals.css";
 import Layout from "../components/layout/layout";
-import Notification from "../components/ui/notification";
 import { NotificationContextProvider } from "../store/notification-context";
 
 function MyApp({ Component, pageProps }) {
@@ -17,7 +16,6 @@ function MyApp({ Component, pageProps }) {
           />
         </Head>
         <Component {...pageProps} />
-        <Notification title="Test" message="This is a test." status="error" />
       </Layout>
     </NotificationContextProvider>
   );
