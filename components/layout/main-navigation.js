4f476cc29cb755090087555fa453f039fae55e4b
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2021-03-14 13:36:29 +0100

    #177

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,8 +1,10 @@
 import Link from "next/link";
+import classes from "./main-navigation.module.css";
+import Logo from "./logo";
 
 function MainNavigation() {
   return (
-    <header>
+    <header className={classes.header}>
       <Link href="/">
         <a>
           <Logo />
