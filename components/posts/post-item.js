13d56c101706903d65f229212d670fb627d762ea
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2021-03-14 14:06:05 +0100

    #179

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -1,5 +1,32 @@
-function () {
+import Link from "next/link";
+import Image from "next/image";
+import classes from "./post-item.module.css";
 
+function PostItem({ post: { title, image, excerpt, date, slug } }) {
+  const formattedDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numberic",
+  });
+
+  const imagePath = `/images/posts/${slug}/${image}`;
+
+  return (
+    <li className={classes.post}>
+      <Link>
+        <a>
+          <div classes={classes.image}>
+            <Image src={imagePath} alt={title} width={300} height={200} />
+          </div>
+          <div classes={classes.content}>
+            <h3>{title}</h3>
+            <time>{formattedDate}</time>
+            <p>{excerpt}</p>
+          </div>
+        </a>
+      </Link>
+    </li>
+  );
 }
 
-export default 
+export default PostItem;
